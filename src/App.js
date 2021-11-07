61d37d7fd5f1fca11b7102ed8f333c7242fd3429
Bagaja <bagajab@gmail.com>
2021-11-07 15:37:47 +0300

    signin component done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Homepage from "./pages/homepage/homepage.component";
 import Shop from "./pages/shop/shop.component";
 import './App.css'
 import Header from "./components/header/header.component";
+import SignInAndSignUp from "./components/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/shop" element={<Shop />} />
+        <Route path="/signin" element={<SignInAndSignUp />} />
       </Routes>
     </div>
   );
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -17,6 +17,9 @@ const Header = () => (
       <Link className="option" to="/contact">
         Contact
       </Link>
+      <Link className="option" to="/signin">
+        Signin
+      </Link>
     </div>
   </div>
 );
