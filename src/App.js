4ae7c20dc75773744e46ec96f4fe43ce0da1113e
Bagaja <bagajab@gmail.com>
2021-11-07 08:06:22 +0300

    added react-router-dom v6

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { Routes, Route, useLocation } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage.component";
 
-function App() {
+const App = () => {
   return (
     <div>
-      <Homepage />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/shop/*" element={<Shop />} />
+      </Routes>
     </div>
   );
-}
+};
+
+const Shop = (props) => {
+  let params = useLocation();
+  return <h1>{params.pathname}</h1>
+};
 
 export default App;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -50,8 +50,8 @@ class Directory extends Component {
 
     return (
       <div className="directory-menu">
-        {sections.map(({ id, imageUrl, title, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
