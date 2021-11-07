965e83347623396a0b29ba42f66f6dfa886cf059
Bagaja <bagajab@gmail.com>
2021-11-07 11:15:08 +0300

    updating app shop page with collection item and preview componenets

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage.component";
+import Shop from "./pages/shop/shop.component";
+import './App.css'
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/shop/*" element={<Shop />} />
+        <Route path="/shop" element={<Shop />} />
       </Routes>
     </div>
   );
-};
-
-const Shop = (props) => {
-  let params = useLocation();
-  return <h1>{params.pathname}</h1>
 };
 
 export default App;
